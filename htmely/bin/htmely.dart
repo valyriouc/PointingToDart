1ee20c12509c3a306bcdf50a75b4427787caad0d
Valacor <valyrious.informatics@gmail.com>
2024-03-10 00:04:03 +0100

    Creating a simple args parser!

--- htmely/bin/htmely.dart
+++ htmely/bin/htmely.dart
@@ -1,5 +1,19 @@
 import 'package:htmely/functions.dart' as htmely;
 
+mixin EmptyMixin on Map<String, String> {
+  static Map<String, String> empty () => {};
+}
+
+typedef CliArgs = Map<String, String>;
+
+CliArgs parseArgs(List<String> args) {
+  for (var i = 0; i < args.length; i++) {
+    
+  }
+  return CliArgs.from({});
+}
+
 void main(List<String> arguments) {
-  
+  final Map<String, String> args = parseArgs(arguments);
+
 }
