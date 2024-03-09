c53def0ec1e152ea91fd28dc17a608f5bd09c028
Valacor <valyrious.informatics@gmail.com>
2024-03-09 19:52:46 +0100

    Starting a markdown to html converter

---
+++ htmely/bin/htmely.dart
@@ -0,0 +1,5 @@
+import 'package:htmely/htmely.dart' as htmely;
+
+void main(List<String> arguments) {
+  print('Hello world: ${htmely.calculate()}!');
+}
---
+++ htmely/test/htmely_test.dart
@@ -0,0 +1,8 @@
+import 'package:htmely/htmely.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
